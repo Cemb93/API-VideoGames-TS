dcba994cf12227a63b1284faeebbb0ef64769655
Cemb93 <karloz13m@gmail.com>
2023-03-08 22:42:06 -0500

    renderizando generos y plataformas en el FRONT

--- client/src/app/components/Game.tsx
+++ client/src/app/components/Game.tsx
@@ -1,13 +1,40 @@
 import React from 'react'
-import { GamesGenres } from '../../../../api/src/interface'
+import { GamesGenres, IGenres } from '../../../../api/src/interface'
 
 export const Game = (
   { id, name, image, released, rating, platforms, genres }: 
   GamesGenres
 ) => {
+  console.log(genres)
   return (
     <div>
       <p>Nombre: {name}</p>
+      <p>Plataformas: {
+        platforms.map((el: string) => {
+          return (
+            <ul key={el} >
+              <li>{`✔ ${el}`}</li>
+            </ul>
+          );
+        })  
+      }</p>
+      <p>Generos: {
+        genres.map((el: string | IGenres, index: number) => (
+        // genres.map((el: string) => (
+            // <ul key={el} >
+            //   <li>{`✔ ${el}`}</li>
+            // </ul>
+          typeof el === 'object' ? (
+            <ul key={index} >
+              <li>DB: {`✔ ${el.name}`}</li>
+            </ul>
+          ) :  (
+            <ul key={index} >
+              <li>API: {`✔ ${el}`}</li>
+            </ul>
+          )
+        ))  
+      }</p>
     </div>
   )
 }
--- client/src/app/components/Games.tsx
+++ client/src/app/components/Games.tsx
@@ -3,11 +3,12 @@
 import { useAppDispatch, useAppSelector } from '@/Hooks'
 import { getAllGames } from '@/redux/Actions'
 import React, { useEffect } from 'react'
-import { GamesGenres } from '../../../../api/src/interface';
+import { GamesGenres, IGenres } from '../../../../api/src/interface';
 import { Game } from './Game';
 
 export const Games = () => {
   const {allGames} = useAppSelector((state) => state)
+  console.log('ALL GAMES',allGames)
   const dispatch = useAppDispatch()
   useEffect(() => {
     dispatch<any>(getAllGames())
@@ -16,6 +17,10 @@ export const Games = () => {
     <div>
       {
         allGames.map((el: GamesGenres) => {
+          let GenresDb = el.genresDb?.map((el: IGenres) => el.name)
+          console.log('GENEROS DB:',GenresDb?.length)
+          let Genres = el.genres.map((el: string) => el)
+          console.log('GENEROS API:', Genres)
           return (
             <div key={el.id} >
               <Game
@@ -24,10 +29,11 @@ export const Games = () => {
                 image={el.image}
                 released={el.released}
                 rating={el.rating}
-                // platforms={!el.platforms.length ? el.platformsDb : el.platforms}
-                platforms={el.platforms}
-                // genres={!el.genres ? el.genresDb : el.genres}
-                genres={el.genres}
+                platforms={!el.platformsDb?.length ? el.platforms : el.platformsDb}
+                // platforms={el.platforms}
+                // genres={!el.genresDb?.length ? el.genres : el.genresDb}
+                genres={!GenresDb?.length ? Genres : GenresDb}
+                // genres={el.genres}
               />
             </div>
           );
