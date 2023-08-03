aed44138a7bb308bec79feb84938e9c0255bcb9e
Cemb93 <karloz13m@gmail.com>
2023-08-03 02:28:48 -0500

    update game

--- api/src/services/Games/UpDateGame.ts
+++ api/src/services/Games/UpDateGame.ts
@@ -1,18 +1,18 @@
 import { Request, Response } from "express";
 import { GamesDb } from "../../../../interface/Games";
 import { VideoGameModel } from "../../models/videogame";
-// import db from "../../models/db";
 
 export const upDateGame = async (req: Request, res: Response) => {
   const { id } = req.params;
   const game: GamesDb = req.body;
+  
   try {
     const current = await VideoGameModel.findById(id);
-    if (!current) {
+    if (current?.id !== id) {
       throw Error('El juego no existe.');
     } else {
-      await VideoGameModel.findByIdAndUpdate(game, { where: { id } } );
-      return res.json({ msg: `El juego ha sido actualizado.` } );
+      await VideoGameModel.findByIdAndUpdate(id, game );
+      return res.json({ msg: `El juego ha sido actualizado.`, game } );
     }
   } catch (error) {
     if (error instanceof Error) {
